46db91ab67b1fe1d750d6b472d4bbc0979fd5fc6
Louis <2720447678@qq.com>
2025-04-23 02:08:32 +0800

    过期时间

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //全局的配置文件
 module.exports = {
   jwtSecretKey: 'louis louis let,s go', //加解密Token的密钥
-  expiresIn: '5s', //token有效期
-  longExpiresIn: '10s', //refreshToken有效期
+  expiresIn: '5m', //token有效期
+  longExpiresIn: '1h', //refreshToken有效期
 }
